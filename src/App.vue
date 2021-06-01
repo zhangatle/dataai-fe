63b73df9bdf6b6a9fef8d6e49af1835dcfc28d27
zhangatle <zhangatle@gmail.com>
2021-06-01 20:30:28 +0800

    backup

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,13 @@ export default {
 }
 </script>
 
-<style scoped>
-#app {
+<style>
+html, body, #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+  height: 100%;
 }
 </style>
--- src/views/result/Index.vue
+++ src/views/result/Index.vue
@@ -1,105 +1,107 @@
 <template>
   <el-container>
-    <el-header>
-      <el-row>
-        <el-col :span="2">
-          <a href="/">
-            <div class="logo">
-              <img src="../../assets/images/logo.png" class="logo" alt="dataai" />
-            </div>
-          </a>
-        </el-col>
-        <el-col :span="6">
-          <el-input type="text" placeholder="请输入关键词进行搜索" v-model="keywords">
-            <el-button slot="append" @click="addSearch">搜索</el-button>
-          </el-input>
-        </el-col>
-      </el-row>
-    </el-header>
-    <el-container>
+    <div style="height: 100%">
+      <el-header>
+        <el-row>
+          <el-col :span="2">
+            <a href="/">
+              <div class="logo">
+                <img src="../../assets/images/logo.png" class="logo" alt="dataai" />
+              </div>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <el-input type="text" placeholder="请输入关键词进行搜索" v-model="keywords">
+              <el-button slot="append" @click="addSearch">搜索</el-button>
+            </el-input>
+          </el-col>
+        </el-row>
+      </el-header>
       <el-container>
-        <el-main>
-          <el-row>
-            <el-col :span="10" :offset="2">
+        <el-container>
+          <el-main>
+            <el-row>
+              <el-col :span="10" :offset="2">
               <span class="summary">找到约&nbsp;<span class="totalResult">{{ total }}</span>&nbsp;条结果(用时<span
                   class="time">{{ last_seconds }}</span>秒)，共约<span class="totalPage">{{ page_nums }}</span>页</span>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :offset="2">
-              <div class="resultList">
-                <div class="resultItem" v-for="(item,index) in hit_list" :key="index">
-                  <div class="itemHead">
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :offset="2">
+                <div class="resultList">
+                  <div class="resultItem" v-for="(item,index) in hit_list" :key="index">
+                    <div class="itemHead">
                             <span class="fileType">
                               <span class="label">来源：</span>
                               <span class="value">{{ item.nickname }}({{ item.wxid }})</span>
                             </span>
-                    <span class="dependValue">
+                      <span class="dependValue">
                               <span class="label">得分：</span>
                               <span class="value">{{ item.score }}</span>
                             </span>
-                  </div>
-                  <div class="itemBody" v-html="item.content"></div>
-                  <div class="itemFoot">
+                    </div>
+                    <div class="itemBody" v-html="item.content"></div>
+                    <div class="itemFoot">
                             <span class="info">
                               <label>发送人：</label>
                               <span class="value">{{ item.message_sender }}</span>
                             </span>
-                    <span class="info">
+                      <span class="info">
                               <label>群昵称：</label>
                               <span class="value">{{ item.message_group }}</span>
                             </span>
-                    <span class="info">
+                      <span class="info">
                               <label>发布时间：</label>
                               <span class="value">{{ item.create_date }}</span>
                             </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </el-col>
-          </el-row>
-          <div class="pagination">
-            <el-row>
-              <el-col :offset="2">
-                <el-pagination
-                    background
-                    layout="prev, pager, next"
-                    :current-page=Number(this.page)
-                    @current-change="changePage"
-                    :page-size="3"
-                    :total="this.total">
-                </el-pagination>
               </el-col>
             </el-row>
-          </div>
-        </el-main>
-        <el-footer>
-          <div class="footer">
-            Copyright 版权所有 E-mail:search@dataai.cn
-          </div>
-        </el-footer>
-      </el-container>
-      <el-aside width="200px">
-        <div class="historyArea">
-          <div class="hotSearch">
-            <h6>热门搜索</h6>
-            <div class="historyList">
+          </el-main>
+        </el-container>
+        <el-aside width="300px">
+          <div class="historyArea">
+            <div class="hotSearch">
+              <h6>热门搜索</h6>
+              <div class="historyList">
               <span  v-for="(item, index) in hot_search" :key="index">
                 <el-link href="/">{{ item }}</el-link><el-divider></el-divider>
               </span>
+              </div>
             </div>
-          </div>
-          <div class="mySearch">
-            <h6>我的搜索</h6>
-            <div class="historyList">
+            <div class="mySearch">
+              <h6>我的搜索</h6>
+              <div class="historyList">
               <span  v-for="(item, index) in searchArr" :key="index">
                 <el-link href="/">{{ item }}</el-link><el-divider></el-divider>
               </span>
+              </div>
             </div>
           </div>
-        </div>
-      </el-aside>
-    </el-container>
+        </el-aside>
+      </el-container>
+    </div>
+    <el-footer>
+      <div class="pagination">
+        <el-row>
+          <el-col :offset="2">
+            <el-pagination
+                background
+                layout="prev, pager, next"
+                :current-page=Number(this.page)
+                @current-change="changePage"
+                :page-size=10
+                :total="this.total">
+            </el-pagination>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="footer">
+        Copyright 版权所有 E-mail:search@dataai.cn
+      </div>
+    </el-footer>
   </el-container>
 </template>
 
@@ -119,18 +121,16 @@ export default {
       page_nums: 0,
       last_seconds: 0,
       hit_list: [],
-      hot_search: ["asdf", "asf"],
-      searchArr: ["sf", "sadf"],
+      hot_search: ["热门词"],
+      searchArr: ["第一"],
     }
   },
 
   methods: {
     getQuery() {
-      console.log(this.$route)
       this.keywords = this.$route.params.q;
     },
     changePage(page) {
-      console.log(page)
       this.page = page
       this.addSearch()
     },
@@ -229,9 +229,20 @@ export default {
 }
 
 .footer{
+  text-align: center;
+}
+
+.historyArea{
   position: absolute;
-  left: 50%;
-  transform: translate(-50%);
-  bottom: 20px;
+  right: 100px;
+  top: 100px;
+  width: 200px;
+  h6{
+    font-size: 18px;
+  }
+  .el-link{
+    color: #0080cc;
+    font-size: 16px;
+  }
 }
 </style>
